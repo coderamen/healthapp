bd9337d2d7d2a2b8393c7fb426b7539f3d950e28
yijerjer <loh.yijer@live.com>
2017-03-09 13:01:40 +0800

    edited out errors in users and sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,40 +1,54 @@
 class UsersController < Clearance::UsersController
 
- def new
-   @user = user.new
- end
+  def new
+    @user = User.new
+  end
 
- def create
-      @user = User.new(user_params)
-   if @user.save
-     log_in @user
-     flash[:success] = "healthapp account has been successfully created"
-     redirect_to @user
-   else
-     render 'new'
-   end
- end
+  def create
+    byebug
+    @user = User.new(user_params)
+    if @user.save
+      sign_in(@user)
+      flash[:success] = "healthapp account has been successfully created"
+      redirect_to root_path
+    else
+      render 'users/new'
+      flash[:danger] = "error in creation of account"
+    end
+  end
 
- def show
-   @user = User.find(params[:id])
- end
+  def show
+    @user = User.find(params[:id])
+  end
 
- def edit
-   @user = User.find(params[:id])
- end
+  def edit
+    @user = User.find(params[:id])
+  end
 
- def update
-   update_hash = update_user_params
-		@user = User.find_by_id(params[:id])
- end
+  def update
+    @user = User.find(params[:id])
 
- def destroy
-   current_user_authorised?(params[:id], user_path(id: params[:id]))
+    if @user.update(user_params)
+      flash[:success] = "Update successful"
+      redirect_to user_path(@user)
+    else
+      flash[:danger] = "Update unsuccessful"
+      redirect_to edit_user_path(@user)
+    end
 
+  end
+
+  def destroy
 		user = User.find_by_id(params[:id])
 		sign_out
 		user.authentications.delete_all
 		user.destroy
 		redirect_to root_path
- end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :name, :city, :state, :country, :age_range, :physique, :additional_health_problems, :weekly_activity_hours)
+  end
 end
