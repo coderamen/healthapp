1ca00462dc846f0978fce86153bbd118ea1a0128
Rails <spencerteh@gmail.com>
2017-03-09 21:53:38 +0800

    front/sidebar profile wip

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 
+  get '/dashboard' => "welcome#dashboard", as: "dashboard"
+
   # routes for user
   resources :users
 
