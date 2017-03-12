cc50987aa7d75fc59c824085450cebfba6b91c6c
yijerjer <loh.yijer@live.com>
2017-03-12 18:50:31 +0800

    is currently working on one side of the user, but no css

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,22 +1,31 @@
-App.messages = App.cable.subscriptions.create({
-    channel: 'MessagesChannel', 
-    match_id: $('.chatbox').data('match-id')
-  }, 
-  {
+$(document).ready(function() {
+
+  App.messages = App.cable.subscriptions.create({channel: 'MessagesChannel', match_id: $('.chatbox').data('match-id')}, {
     received: function(data) {
+      var $newMessage = $('<li><p></p></li>');
 
-  }
-});
+      if (data.user_id === $('.chatbox').data('self')) {
+        $newMessage.addClass('self');
+      }
+      else if (data.user_id === $('.chatbox').data('other')) {
+        $newMessage.addCLass('other');
+      }
 
-$(document).on('turbolinks:load', function() {
-  $('.reply-msg > form').submit(function() {
-    var msg = $(this).children('#message_content').val();
-    var matchId = $('.chatbox').data('.match-id');
-    var userId = $('.chatbox').data('self');
+      $newMessage.html('<p>' + data.message + '</p>');
+      $('ol.chat').append($newMessage);
+    }
+  });
 
-    App.messages.send({content: msg, match_id: matchId, user_id: userId});
-    $(this).children('#message_content').val('');
+  $(document).on('turbolinks:load', function() {
+    $('.reply-msg > form').submit(function() {
+      var msg = $(this).children('#message_content').val();
+      var matchId = $('.chatbox').data('match-id');
+      var userId = $('.chatbox').data('self');
 
-    return false;
+      App.messages.send({content: msg, match_id: matchId, user_id: userId});
+      $(this).children('#message_content').val('');
+
+      return false;
+    });
   });
 });
--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -3,4 +3,9 @@ class MessagesChannel < ApplicationCable::Channel
     stream_from "messages_from_match#{params[:match_id]}"
   end
 
+  def receive(data)
+    message = Message.create(match_id: data["match_id"], user_id: data["user_id"], content: data["content"])
+    ActionCable.server.broadcast("messages_from_match#{params[:match_id]}", { message: message.content, user_id: message.user_id })
+  end
+
 end
