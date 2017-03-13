c17c8c2f7a1df34faa2d09e98672be9d69168863
yijerjer <loh.yijer@live.com>
2017-03-13 23:33:04 +0800

    added panels, made confirmed_activity form with css

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   constraints Clearance::Constraints::SignedIn.new do
     root "welcome#dashboard"
-    # root 'welcome#dashboard'
   end
 
   constraints Clearance::Constraints::SignedOut.new do
