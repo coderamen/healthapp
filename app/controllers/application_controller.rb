0d71ccf06d5bb0a0dc5947883bfc33776d2cab84
yijerjer <loh.yijer@live.com>
2017-03-15 16:23:43 +0800

    fix create pending date issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,28 +34,16 @@ class ApplicationController < ActionController::Base
 
   # get datetime object from separated date and time values
   def get_datetime(params)
-    date = params[:date]
-    time = params[:time]
+    byebug
+    date = params[:date].to_date
+    time = params[:time].to_time
 
-    day = date[0..1].to_i
-    month = date[3..4].to_i
-    year = date[6..9].to_i
+    day = date.day
+    month = date.month
+    year = date.year
 
-    if time.length == 8
-      hour = time[0..1].to_i
-      min = time[3..4].to_i
-
-      if time[6..7] == "PM"
-        hour += 12
-      end
-    elsif time.length == 7
-      hour = time[0].to_i
-      min = time[2..3].to_i
-
-      if time[5..6] == "PM"
-        hour += 12
-      end
-    end
+    hour = time.hour
+    min = time.min
 
     Time.local(year, month, day, hour, min, 0)
   end
